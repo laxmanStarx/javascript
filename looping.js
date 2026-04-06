32044bbf59f255a627847fc7ae050b089442bec0
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-04-06 23:34:36 +0530

    first commits

--- looping.js
+++ looping.js
@@ -1,3 +1,10 @@
-for(let i = 0; i < 40;i++){
-    console.log(i)
+let sum = 0;
+
+let n = 50
+
+
+for(let i = 0; i < n;i++){
+    sum +=(i)
 }
+
+console.log("sum of first " + n + " natural numbers is " + sum)
