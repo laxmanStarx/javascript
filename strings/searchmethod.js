96defcd2f9be38fb24826f6b763add75a354a549
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-04-10 23:55:00 +0530

    first commitsdshlksskjkkk

---
+++ strings/searchmethod.js
@@ -0,0 +1,11 @@
+let str = "hello world";
+
+str.indexOf("o");      
+str.lastIndexOf("o");  
+
+str.includes("world"); // true
+str.startsWith("he");  
+str = str.endsWith("ld");    
+
+console.log(str);
+
