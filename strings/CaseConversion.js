d70e1b6f95fcf93e957dc60bb54656a42015c2ec
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-04-10 23:41:48 +0530

    first commitsdshlksskjkk

---
+++ strings/CaseConversion.js
@@ -0,0 +1,14 @@
+
+
+let str3 = "HELLO";
+
+str3 = str3.toLowerCase();
+
+console.log(str3);
+
+let str = "hellso";
+
+str = str.toUpperCase(); 
+console.log(str);
+// str.toLowerCase(); 
+// console.log(str)
