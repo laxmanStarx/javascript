630c63c75f76c958b51e7e4759e8d27124affa4d
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-04-18 00:58:14 +0530

    second commitsdhgfljlldlkl

---
+++ strings/stringslice.js
@@ -0,0 +1,11 @@
+let text = "apple, Banana, kiwi";
+
+let part = text.slice(7,13);
+
+console.log(part)
+
+let fruit = "Apple, Banana, Kiwi";
+
+let parts = fruit.slice(-12,-6)
+
+console.log(parts)
