d7c810b864b6b372bef703d2311d6b1995816da4
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-04-18 00:32:10 +0530

    second commitsdhgfljlldlk

---
+++ strings/stringmethod.js
@@ -0,0 +1,15 @@
+const name = "W3schools";
+
+let letter = name.at(4);
+
+console.log(letter)
+
+
+//another way of excessinf the word
+
+
+const names = "ShanksGojo"
+
+let letters = names[4]
+
+console.log(letters)
