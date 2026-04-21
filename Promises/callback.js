8aaaee370d9d310ead7dd025cc9b296e9e826d82
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-04-22 01:02:33 +0530

    first commitsmilkjlkll

--- Promises/callback.js
+++ Promises/callback.js
@@ -1,8 +1,35 @@
-function processUserInput(callback) {
-    let name = "Vaibhav"
-    callback(name)
-}
+// function processUserInput(callback) {
+//     let name = "Vaibhav"
+//     callback(name)
+// }
 
-processUserInput(function(name) {
-    console.log("Hello " + name)
-})
+// processUserInput(function(name) {
+//     console.log("Hello " + name)
+// })
+
+// function calculate(a, b, operation) {
+//     return operation(a, b)
+// }
+
+// function add(x, y) {
+//     return x + y
+// }
+
+// function multiply(x, y) {
+//     return x * y
+// }
+
+// console.log(calculate(2, 3, add))       // 5
+// console.log(calculate(2, 3, multiply))  // 6
+
+
+
+setTimeout(() => {
+    console.log("Step 1")
+    setTimeout(() => {
+        console.log("Step 2")
+        setTimeout(() => {
+            console.log("Step 3")
+        }, 1000)
+    }, 1000)
+}, 1000)
